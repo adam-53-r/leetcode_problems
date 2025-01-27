7905c5b41c35e9557214c416d7f744bf51407fd1
Adam Rkouni <adam-53-r@protonmail.com>
2025-01-27 12:32:19 +0100

    Added devenv and idea config

---
+++ devenv.nix
@@ -0,0 +1,27 @@
+{ pkgs, lib, ... }:
+
+{
+  packages = with pkgs; [
+    openssl
+    rustup
+  ];
+
+  languages.rust = {
+    enable = true;
+    # https://devenv.sh/reference/options/#languagesrustchannel
+    channel = "stable";
+    # channel = "nightly";
+
+    components = [ "rustc" "cargo" "clippy" "rustfmt" "rust-analyzer" "rust-std" "rust-src" ];
+    targets = [ "x86_64-unknown-linux-musl" ];
+  };
+
+  # pre-commit.hooks = {
+  #  rustfmt.enable = true;
+  #  clippy.enable = true;
+  # };
+  
+  enterShell = ''
+    exec fish
+  '';
+}
